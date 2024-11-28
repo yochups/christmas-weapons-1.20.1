403760f775d50e330dbf8f481a6cfff2d304352e
yochups <yochupog@gmail.com>
2024-11-28 16:30:03 +0100

    all

---
+++ src/main/java/net/yochu/christmas/ChristmasWeaponsClient.java
@@ -0,0 +1,10 @@
+package net.yochu.christmas;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChristmasWeaponsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
